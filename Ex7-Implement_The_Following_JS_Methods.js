3ef1b803a7bad5e6b6d8477b56faee57d474303f
EyalSinay <eyalsinay@gmail.com>
2022-04-14 11:44:44 +0300

    create all files

---
+++ Ex7-Implement_The_Following_JS_Methods.js
@@ -0,0 +1,12 @@
+// Optional!
+
+/*
+Ex7 - Implement The Following JS Methods -
+Implement the following methods -
+- Filter
+- ForEach
+- Map
+Using only for(), array and objects (without other js methods)
+Find Bugs \ Debug Code
+Bug !!!!!
+*/
